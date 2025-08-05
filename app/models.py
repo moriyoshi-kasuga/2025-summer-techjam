b17cb442f0b40d30ec7f2eae07ff6e9d1b4301e9
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-05 09:31:50 +0900

    fix: change Post content from String to Text

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class Admin(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    description = db.Column(db.String(512), nullable=True)
+    content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
 
     author_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
