8e44d8a9efe8289641978f1ffba86288e4b9656e
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-06 07:57:57 +0900

    style: format home.py

--- app/views/home.py
+++ app/views/home.py
@@ -1,10 +1,17 @@
-from flask import Blueprint, render_template
 import random
+
+from flask import Blueprint, render_template
 
 blueprint = Blueprint("home", __name__)
 
 
 @blueprint.route("/")
 def home():
-    words = ["きょうは、はなび大会にいきました。","きょうは、おばあちゃんちにいきました。","きょうは、カブトムシをつかまえました。","きょうは、プールにいきました。","きょうは、ながしそうめんをしました。"]
-    return render_template("index.html",word=random.choice(words))
+    words = [
+        "きょうは、はなび大会にいきました。",
+        "きょうは、おばあちゃんちにいきました。",
+        "きょうは、カブトムシをつかまえました。",
+        "きょうは、プールにいきました。",
+        "きょうは、ながしそうめんをしました。",
+    ]
+    return render_template("index.html", word=random.choice(words))
