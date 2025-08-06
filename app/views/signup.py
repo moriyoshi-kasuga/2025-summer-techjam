ea18d38f3010ecc51b749a2dfc96c214e3a69834
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-06 13:04:38 +0900

    fix: signup password

--- app/views/signup.py
+++ app/views/signup.py
@@ -13,14 +13,21 @@ def signup():
         return render_template("signup.html")
     email = request.form.get("email")
     username = request.form.get("username")
+    password = request.form.get("password")
+    if password is None:
+        return render_template("signup.html", error="パスワードを入力してください")
+    confirm_password = request.form.get("confirm_password")
+    if password != confirm_password:
+        return render_template("signup.html", error="パスワードが一致しません")
     user: User | None = User.query.filter(User.email == email).first()
     if user:
         return render_template("signup.html", error="入力されたemailは既に登録済みです")
 
-    user = User(username=username, email=email)
+    user = User(name=username, email=email, password=password)
     db.session.add(user)
     db.session.commit()
 
     login_user(user)
 
     return redirect("/create")
+
