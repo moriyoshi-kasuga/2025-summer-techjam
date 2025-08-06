a82bc021d3ef7e66597e95c4eb3591ace18d8355
hayato <hayato.a.0805@gmail.com>
2025-08-06 11:38:38 +0900

    feat: エラー文追加

--- app/views/login.py
+++ app/views/login.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request
+from flask_login import login_user
+
+from app.models import User
 
 blueprint = Blueprint("login", __name__)
 
 
-@blueprint.route("/")
+@blueprint.route("/", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    user: User | None = User.query.filter(User.email == email).first()
+    if user is None:
+        return render_template("login.html", error="入力されたemailはアカウント登録されていません")
+
+    if user.password != password:
+        return render_template("login.html",error="パスワードが間違っています。再度ご入力ください。")
+
+    login_user(user)
+
+    return redirect("/mypage")
