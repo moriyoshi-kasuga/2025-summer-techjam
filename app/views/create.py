f774cdb55930cdbe02de49e88a017cb802a94f25
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-06 14:47:12 +0900

    feat: /create のpython実装

--- app/views/create.py
+++ app/views/create.py
@@ -1,8 +1,19 @@
+from datetime import datetime
+
 from flask import Blueprint, render_template
+from flask_login import current_user, login_required
+
+from app.models import User
 
 blueprint = Blueprint("create", __name__)
 
 
 @blueprint.route("/")
+@login_required
 def create():
-    return render_template("create.html")
+    id = current_user.id
+    username = User.query.get(id).name
+    date = datetime.now()
+    return render_template(
+        "create.html", username=username, month=date.month, day=date.day
+    )
