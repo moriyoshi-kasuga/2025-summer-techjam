dae2961d1680635002c33f66576470d58307578d
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-06 12:19:01 +0900

    feat: mypageのpython実装

--- app/views/mypage.py
+++ app/views/mypage.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, render_template
+from datetime import datetime
+from typing import List
+
+from flask import Blueprint, redirect, render_template
+from flask_login import current_user
+
+from app.models import Post, User
 
 blueprint = Blueprint("mypage", __name__)
 
 
 @blueprint.route("/")
 def mypage():
-    return render_template("mypage.html")
+    if current_user is None:
+        return redirect("/login")
+    user: User = current_user
+    posts: List[Post] = (
+        Post.query.filter(Post.author_id == user.id).order_by(Post.created_at).all()
+    )
+    formatted = []
+    for post in posts:
+        date: datetime = post.created_at
+        formatted.append({"id": post.id, "date": f"{date.month}月{date.day}日"})
+    return render_template("mypage.html", posts=formatted)
