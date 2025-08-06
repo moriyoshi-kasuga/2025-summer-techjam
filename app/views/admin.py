97e079b8f02b6a969845d8e5a6d3914fde3286f6
asa-arch10 <asahi.kawamura0811@gmail.com>
2025-08-06 11:20:45 +0900

    feat:adminページの追加

--- app/views/admin.py
+++ app/views/admin.py
@@ -5,4 +5,11 @@ blueprint = Blueprint("admin", __name__)
 
 @blueprint.route("/")
 def admin():
-    return render_template("admin.html")
+    test_data = [
+        ["0801", "たけし", "takesi@gmail.com", "ともひさ"],
+        ["0830", "ゆみこ", "superyumiko@gmail.com", "マイケルたけし"],
+    ]
+    if len(test_data) < 10:
+        for _ in range(20 - len(test_data)):
+            test_data.append(["0000", " ", " ", " "])
+    return render_template("admin.html", datas=test_data)
