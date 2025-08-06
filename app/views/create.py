bd9f64d7be8fda6ea36d07d15a5308c4acf0c7d7
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-06 12:32:17 +0900

    feat: impl create.py

--- app/views/create.py
+++ app/views/create.py
@@ -1,8 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request
+from flask_login import current_user
+
+from app import db
+from app.models import Post, User
+from app.views.image import save_image
 
 blueprint = Blueprint("create", __name__)
 
 
-@blueprint.route("/")
+@blueprint.route("/", methods=["GET", "POST"])
 def create():
-    return render_template("create.html")
+    if current_user is None:
+        return redirect("/login")
+    user: User = current_user
+    if request.method == "GET":
+        return render_template("create.html")
+    content = request.form.get("content")
+    image = request.files.get("image")
+    if content is None or len(content) == 0:
+        return render_template("create.html")
+    post = Post(content=content, author_id=user.id)
+    db.session.add(post)
+    db.session.commit()
+    save_image(post.id, image)
+    return redirect("/view")
